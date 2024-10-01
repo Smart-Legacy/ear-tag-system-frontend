e798378a0bcb669bcf36b3a6002dbafdacb9a3a8
fraolb <Fraolbereket@gmail.com>
2024-10-01 13:34:12 +0300

    customized themed view

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,30 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  otherStyles?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({
+  style,
+  lightColor,
+  otherStyles,
+  darkColor,
+  ...otherProps
+}: ThemedViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <View
+      className={`${otherStyles}`}
+      style={[{ backgroundColor }, style]}
+      {...otherProps}
+    />
+  );
 }
