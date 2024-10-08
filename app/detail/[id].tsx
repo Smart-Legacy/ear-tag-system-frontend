af4ceb27d6fe83bd2c2d2d46c5f08a9130b9402d
fraolb <Fraolbereket@gmail.com>
2024-10-08 20:26:30 +0300

    light and dark mode

--- app/detail/[id].tsx
+++ app/detail/[id].tsx
@@ -1,38 +1,72 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { useColorScheme } from "react-native";
 
 import Cow from "@/assets/icons/cowWhite.png";
+import CowBlack from "@/assets/icons/cow.png";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
 
 const DetailPage = () => {
+  const theme = useColorScheme() ?? "light";
+
   return (
-    <View className="flex-1 p-4 bg-gray-900">
+    <ThemedView className="flex-1 p-4">
       <ScrollView className="space-y-4 mt-4">
         <View className="flex-row w-11/12 justify-between mt-5 mb-5 items-center">
           <Image
-            source={Cow}
+            source={theme == "light" ? CowBlack : Cow}
             resizeMode="contain"
             className="w-[80px] h-[80px]"
           />
-          <Text className="text-xl text-white font-bold w-[70%]">
-            Cattle A113
-          </Text>
+          <ThemedText
+            lightColor="black"
+            darkColor="white"
+            className="text-xl font-bold w-[70%]"
+          >
+            Cattle A113 {theme}
+          </ThemedText>
         </View>
 
         {/* 2x2 Grid for Cattle Details */}
         <View className="flex flex-wrap flex-row justify-between">
-          <View className="w-[48%] bg-gray-800 p-2 mb-2 rounded-md">
-            <Text className="text-white">Id: u29328420</Text>
-          </View>
-          <View className="w-[48%] bg-gray-800 p-2 mb-2 rounded-md">
-            <Text className="text-white">Sex: Female</Text>
-          </View>
-          <View className="w-[48%] bg-gray-800 p-2 mb-2 rounded-md">
-            <Text className="text-white">Last Medication: -</Text>
-          </View>
-          <View className="w-[48%] bg-gray-800 p-2 mb-2 rounded-md">
-            <Text className="text-white">Latest Weight: 55 kg</Text>
-          </View>
+          <ThemedView
+            lightColor="rgb(229 231 235)"
+            darkColor="rgb(55 65 81)"
+            className="w-[48%] p-2 mb-2 rounded-md"
+          >
+            <ThemedText lightColor="black" darkColor="white">
+              Id: u29328420
+            </ThemedText>
+          </ThemedView>
+          <ThemedView
+            lightColor="rgb(229 231 235)"
+            darkColor="rgb(55 65 81)"
+            className="w-[48%] p-2 mb-2 rounded-md"
+          >
+            <ThemedText lightColor="black" darkColor="white">
+              Sex: Female
+            </ThemedText>
+          </ThemedView>
+          <ThemedView
+            lightColor="rgb(229 231 235)"
+            darkColor="rgb(55 65 81)"
+            className="w-[48%] p-2 mb-2 rounded-md"
+          >
+            <ThemedText lightColor="black" darkColor="white">
+              Last Medication: -
+            </ThemedText>
+          </ThemedView>
+          <ThemedView
+            lightColor="rgb(229 231 235)"
+            darkColor="rgb(55 65 81)"
+            className="w-[48%] p-2 mb-2 rounded-md"
+          >
+            <ThemedText lightColor="black" darkColor="white">
+              Latest Weight: 55 kg
+            </ThemedText>
+          </ThemedView>
         </View>
 
         {/* Buttons with chevron-forward icon */}
@@ -47,16 +81,32 @@ const DetailPage = () => {
         </TouchableOpacity>
 
         {/* Displaying Events */}
-        <View className="mt-6">
-          <Text className="text-lg text-white font-bold mb-2">Events</Text>
-          <View className="bg-gray-800 p-4 rounded-lg mb-2">
-            <Text className="text-white">Last Medicated: N/A</Text>
-            <Text className="text-white">Weight Recorded: 55 kg</Text>
-            <Text className="text-white">Other Event: None</Text>
-          </View>
-        </View>
+        <ThemedView className="mt-6">
+          <ThemedText
+            lightColor="black"
+            darkColor="white"
+            className="text-lg font-bold mb-2"
+          >
+            Events
+          </ThemedText>
+          <ThemedView
+            lightColor="rgb(229 231 235)"
+            darkColor="rgb(55 65 81)"
+            className="p-4 rounded-lg mb-2"
+          >
+            <ThemedText lightColor="black" darkColor="white">
+              Last Medicated: N/A
+            </ThemedText>
+            <ThemedText lightColor="black" darkColor="white">
+              Weight Recorded: 55 kg
+            </ThemedText>
+            <ThemedText lightColor="black" darkColor="white">
+              Other Event: None
+            </ThemedText>
+          </ThemedView>
+        </ThemedView>
       </ScrollView>
-    </View>
+    </ThemedView>
   );
 };
 
