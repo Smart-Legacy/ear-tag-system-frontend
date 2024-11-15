fdaab196c7f6112eb8346cee131d3b2b8e91670c
fraolb <Fraolbereket@gmail.com>
2024-11-15 11:40:43 +0300

    update the tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,15 +4,29 @@ import React from "react";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Platform } from "react-native";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const theme = useColorScheme() ?? "light";
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {
+            backgroundColor: theme === "light" ? "white" : "black",
+          },
+        }),
       }}
     >
       <Tabs.Screen
